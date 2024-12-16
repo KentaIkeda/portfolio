2d89f8bea85dcf301d9d3e041353c687f9904d71
Kenta Ikeda <77259023+Krudaovo@users.noreply.github.com>
2024-12-17 04:55:02 +0900

    change to display my name in Information.tsx

--- app/_components/_sections/_Information/Information.tsx
+++ app/_components/_sections/_Information/Information.tsx
@@ -1,32 +1,41 @@
-import React from 'react'
-import {SNSInfo} from "@/lib/SNSInfo";
+import React from 'react';
+import { SNSInfo } from '@/lib/SNSInfo';
 
 const Information = () => {
+  const name =
+    process.env.NODE_ENV === 'development'
+      ? process.env.NEXT_PUBLIC_MY_NAME
+      : process.env.NODE_ENV === 'production'
+      ? process.env.MY_NAME
+      : null;
   return (
-    <section id={'information'} className={'space-y-8'}>
+    <section
+      id={'information'}
+      className={'space-y-8'}
+    >
       <article className={'text-justify text-sm rounded-[5px] p-4 space-y-2.5'}>
-        <p>ようこそ、<span className={'font-semibold blur'}>{process.env.NEXT_PUBLIC_MY_NAME}</span>と申します。1996年2月5日
-          <wbr/>
+        <p>
+          ようこそ、<span className={'font-semibold blur'}>{name}</span>
+          と申します。1996年2月5日
+          <wbr />
           に生まれ、現在はフロントエンドエンジニアとして活動をしています。
         </p>
-        <p>AIが発達した今、AIではなく人間がエンドユーザーを念頭に置いて開発することに意味がある。と私は考えます。</p>
+        <p>
+          AIが発達した今、AIではなく人間がエンドユーザーを念頭に置いて開発することに意味がある。と私は考えます。
+        </p>
       </article>
       <article id={'SNS'}>
         <ul className={'flex justify-center gap-x-4'}>
-          {
-            SNSInfo.map((info) => {
-              return (
-                <li key={info.title}>
-                  <a href={info.href}>
-                    {info.icon}
-                  </a>
-                </li>
-              )
-            })
-          }
+          {SNSInfo.map(info => {
+            return (
+              <li key={info.title}>
+                <a href={info.href}>{info.icon}</a>
+              </li>
+            );
+          })}
         </ul>
       </article>
     </section>
-  )
-}
-export default Information
+  );
+};
+export default Information;
