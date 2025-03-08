b48803b6a4855b34c2470a4e1543d1b952d49a50
Kenta Ikeda <77259023+Krudaovo@users.noreply.github.com>
2025-03-08 18:40:03 +0900

    resolve build error

--- app/_components/root/blog/Blog.tsx
+++ app/_components/root/blog/Blog.tsx
@@ -3,7 +3,6 @@ import type { BlogContent, BreadcrumbItem, CategoryContent } from "@/app/types/t
 import Breadcrumb from "../../Breadcrumb";
 import CategoryList from "../../CategoryList";
 import BlogList from "../../BlogList";
-import Loading from "../../Skeleton/Loading";
 
 interface Props {
   allBlog: BlogContent[];
